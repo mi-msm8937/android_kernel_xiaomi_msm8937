94d951c342199e2b2380463732276d9fcc864f72
Ryan Andri <ryanandri@linuxmail.org>
2017-12-29 04:34:38 +0700

    input: dt2w/s2w: hook screens status to ft5336 ts driver
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- drivers/input/touchscreen/ft5336/ft5x06_720p.c
+++ drivers/input/touchscreen/ft5336/ft5x06_720p.c
@@ -38,6 +38,18 @@
 #include <linux/hardware_info.h>
 #endif
 
+#if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) && \
+	 !defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
+#include <linux/input/doubletap2wake.h>
+#else if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE) && \
+	 !defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
+#include <linux/input/sweep2wake.h>
+#else if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) && \
+	 defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
+#include <linux/input/doubletap2wake.h>
+#include <linux/input/sweep2wake.h>
+#endif
+
 #if CTP_CHARGER_DETECT
 #include <linux/power_supply.h>
 #endif
@@ -695,6 +707,7 @@ static int ft5x06_ts_resume(struct device *dev)
 			dev_err(dev, "power on failed");
 			return err;
 		}
+
 	}
 
 	if (gpio_is_valid(data->pdata->reset_gpio)) {
@@ -770,9 +783,35 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK &&
 			ft5x06_data && ft5x06_data->client) {
 		blank = evdata->data;
-		if (*blank == FB_BLANK_UNBLANK)
-				schedule_work(&ft5x06_data->fb_notify_work);
-		else if (*blank == FB_BLANK_POWERDOWN) {
+
+		if (*blank == FB_BLANK_UNBLANK
+				|| *blank == FB_BLANK_NORMAL
+				|| *blank == FB_BLANK_VSYNC_SUSPEND) {
+			#if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) && \
+				!defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
+					dt2w_scr_suspended = false;
+			#else if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE) && \
+				!defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
+					s2w_scr_suspended = false;
+			#else if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) && \
+				defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
+					dt2w_scr_suspended = false;
+					s2w_scr_suspended = false;
+			#endif
+			schedule_work(&ft5x06_data->fb_notify_work);
+		} else if (*blank == FB_BLANK_POWERDOWN) {
+			#if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) && \
+				!defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
+					dt2w_scr_suspended = true;
+			#else if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE) && \
+				!defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
+					s2w_scr_suspended = true;
+			#else if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) && \
+				defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
+					dt2w_scr_suspended = true;
+					s2w_scr_suspended = true;
+			#endif
+
 			flush_work(&ft5x06_data->fb_notify_work);
 			ft5x06_ts_suspend(&ft5x06_data->client->dev);
 		}
