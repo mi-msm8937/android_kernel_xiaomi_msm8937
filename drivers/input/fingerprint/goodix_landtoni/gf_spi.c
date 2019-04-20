e806da3f135a8a8f99866aec524952767a8c6320
Nich <nctrenco@gmail.com>
2019-04-20 13:57:54 +0000

    fingerprint: goodix_landtoni: gf_spi: Dynamically allocate a major number

--- drivers/input/fingerprint/goodix_landtoni/gf_spi.c
+++ drivers/input/fingerprint/goodix_landtoni/gf_spi.c
@@ -67,7 +67,6 @@ extern int xiaomi_series_read(void);
 
 #define CHRD_DRIVER_NAME		"goodix_fp_spi"
 #define CLASS_NAME		"goodix_fp"
-#define SPIDEV_MAJOR		225
 #define N_SPI_MINORS		32
 
 
@@ -109,6 +108,7 @@ static LIST_HEAD(device_list);
 static DEFINE_MUTEX(device_list_lock);
 static struct gf_dev gf;
 static struct class *gf_class;
+static unsigned int gf_major = -1;
 static int driver_init_partial(struct gf_dev *gf_dev);
 
 static void gf_enable_irq(struct gf_dev *gf_dev)
@@ -684,7 +684,7 @@ static int gf_probe(struct platform_device *pdev)
 	if (minor < N_SPI_MINORS) {
 		struct device *dev;
 
-		gf_dev->devt = MKDEV(SPIDEV_MAJOR, minor);
+		gf_dev->devt = MKDEV(gf_major, minor);
 		dev = device_create(gf_class, &gf_dev->spi->dev, gf_dev->devt,
 				    gf_dev, GF_DEV_NAME);
 		status = IS_ERR(dev) ? PTR_ERR(dev) : 0;
@@ -863,15 +863,16 @@ static int __init gf_init(void)
 	 */
 
 	BUILD_BUG_ON(N_SPI_MINORS > 256);
-	status = register_chrdev(SPIDEV_MAJOR, CHRD_DRIVER_NAME, &gf_fops);
-	if (status < 0) {
+	/* Dynamically allocate a major */
+	gf_major = register_chrdev(0, CHRD_DRIVER_NAME, &gf_fops);
+	if (gf_major < 0) {
 		pr_warn("Failed to register char device!\n");
 		FUNC_EXIT();
 		return status;
 	}
 	gf_class = class_create(THIS_MODULE, CLASS_NAME);
 	if (IS_ERR(gf_class)) {
-		unregister_chrdev(SPIDEV_MAJOR, gf_driver.driver.name);
+		unregister_chrdev(gf_major, gf_driver.driver.name);
 		pr_warn("Failed to create class.\n");
 		FUNC_EXIT();
 		return PTR_ERR(gf_class);
@@ -883,7 +884,7 @@ static int __init gf_init(void)
 #endif
 	if (status < 0) {
 		class_destroy(gf_class);
-		unregister_chrdev(SPIDEV_MAJOR, gf_driver.driver.name);
+		unregister_chrdev(gf_major, gf_driver.driver.name);
 		pr_warn("Failed to register SPI driver.\n");
 	}
 
@@ -921,7 +922,8 @@ static void __exit gf_exit(void)
 	spi_unregister_driver(&gf_driver);
 #endif
 	class_destroy(gf_class);
-	unregister_chrdev(SPIDEV_MAJOR, gf_driver.driver.name);
+	if (gf_major >= 0)
+		unregister_chrdev(gf_major, gf_driver.driver.name);
 	FUNC_EXIT();
 }
 
