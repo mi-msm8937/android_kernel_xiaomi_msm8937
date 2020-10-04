d8d1033d87dcf344e281095496c8bdee24a22a5b
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-10-04 05:44:45 +0530

    input: touchscreen: gt9xx_v2.8: disable debug

--- drivers/input/touchscreen/gt9xx_v2.8/gt9xx.h
+++ drivers/input/touchscreen/gt9xx_v2.8/gt9xx.h
@@ -64,7 +64,7 @@
 #define GTP_ADDR_LENGTH       2
 
 /***************************PART1:ON/OFF define*******************************/
-#define GTP_DEBUG_ON          1
+#define GTP_DEBUG_ON          0
 #define GTP_DEBUG_ARRAY_ON    0
 #define GTP_DEBUG_FUNC_ON     0
 
