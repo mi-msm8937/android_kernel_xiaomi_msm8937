60a11f04e6bd1d31b34401a29de9eeb968823b3b
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-24 15:49:31 +0800

    techpack: audio: define gesture_incall bool for dt2w/s2w incase of not compiling audio

--- techpack/audio/no_techpack_audio.c
+++ techpack/audio/no_techpack_audio.c
@@ -1,6 +1,10 @@
 #include <linux/printk.h>
 #include <linux/module.h>
 
+#if (defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) || defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE))
+bool gesture_incall = false;
+#endif
+
 static int no_techpack_audio_init(void)
 {
 	pr_info("techpack/audio isn't compiled in!");
