431403578e0a56897d65ec2bacf055b74b5addd7
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-02-08 16:39:36 +0800

    Add script for building releases automatically

---
+++ build-release.sh
@@ -0,0 +1,319 @@
+#!/bin/bash -e
+set +x
+
+MY_PATH=$(dirname $(realpath "$0"))
+cd $MY_PATH
+
+BRANCH_COMMITS="reference"
+BRANCH_MAIN="a12/master"
+BUILD_FLAGS="-j$(nproc) LLVM=1"
+COMMITMSG_LEGACY_OMX="This reverts commit e265d46203a6a01abb9824933dee5641f4aff428"
+COMMITMSG_LEGACY_VIB_GPIO="Squashed Revert of LED GPIO vibrator driver"
+COMMITMSG_LEGACY_VIB_QPNP="Squashed Revert of QTI Haptics commits"
+SUPPORTED_ANDROID_VERSIONS="8.1.0 - 12"
+
+if ! git --version > /dev/null; then
+    echo "Git does not work."
+    exit 1
+fi
+if ! [ -d ".git" ] || ! [ -d ".ak3" ];then
+    echo "The tree is broken."
+    exit 1
+fi
+
+func_help() {
+    echo "Build script of Mi8937 Kernel"
+    echo
+    echo "Parameters or Environment variables:"
+    echo " Required:"
+    echo "  --device | DEVICE"
+    echo "  --out | OUT"
+    echo "  --llvm-path | LLVM_PATH"
+    echo
+    echo " Optional:"
+    echo "  --artifact-copy | ARTIFACT_COPY"
+    echo "  --artifact-upload | ARTIFACT_UPLOAD"
+    echo "  --cherry-pick | CHERRY_PICK"
+    echo "  --legacy-omx | LEGACY_OMX"
+    echo "  --legacy-vib | LEGACY_VIB"
+    echo "  --lto | LTO"
+    echo "  --partition | PARTITION"
+}
+
+func_get_commitid_by_msg() {
+    git log --grep="$1" $BRANCH_COMMITS 2>/dev/null|head -n 1|cut -c 8-
+}
+
+func_set_defconfig() {
+    cfg="$1"
+    item="$2"
+    value="$3"
+    sed -i "/${item}=/d" $cfg
+    echo "${item}=${value}" >> $cfg
+}
+
+func_validate_parameter_value() {
+	case "${2}" in
+		-*)
+			echo "Invalid value for parameter ${1}: ${2}"
+            exit 1
+			;;
+	esac
+}
+
+if [ "$#" -eq 0 ]; then
+    func_help
+    exit 0
+fi
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        # Required
+        --device )
+            func_validate_parameter_value "${1}" "${2}"
+            DEVICE="${2}"
+            shift
+            shift
+            ;;
+        --out )
+            func_validate_parameter_value "${1}" "${2}"
+            OUT="${2}"
+            shift
+            shift
+            ;;
+        --llvm-path )
+            func_validate_parameter_value "${1}" "${2}"
+            LLVM_PATH="${2}"
+            shift
+            shift
+            ;;
+        # Optional
+        --artifact-copy )
+            func_validate_parameter_value "${1}" "${2}"
+            ARTIFACT_COPY="${2}"
+            shift
+            shift
+            ;;
+        --artifact-upload )
+            func_validate_parameter_value "${1}" "${2}"
+            ARTIFACT_UPLOAD="${2}"
+            shift
+            shift
+            ;;
+        --cherry-pick )
+            func_validate_parameter_value "${1}" "${2}"
+            CHERRY_PICK="${2}"
+            shift
+            shift
+            ;;
+        --legacy-omx )
+            LEGACY_OMX="true"
+            shift
+            ;;
+        --legacy-vib )
+            LEGACY_VIB="true"
+            shift
+            ;;
+        --lto )
+            LTO="true"
+            shift
+            ;;
+        --partition )
+            func_validate_parameter_value "${1}" "${2}"
+            PARTITION="${2}"
+            shift
+            shift
+            ;;
+        --help )
+            func_help
+            exit 0
+            ;;
+        -*)
+            echo "Unknown parameter: $1"
+            exit 1
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+if ! [ "$(git branch --list $BRANCH_COMMITS)" ]; then
+    echo "Error: Branch $BRANCH_COMMITS does not exist, Which is required."
+    exit 1
+fi
+if ! [ "$(git branch --list $BRANCH_MAIN)" ]; then
+    echo "Error: Branch $BRANCH_MAIN does not exist, Which is required."
+    exit 1
+fi
+
+case "$DEVICE" in
+    "mi8937"|"mi8937-all"|"riva"|"tiare")
+        ;;
+    *)
+        echo "Unsupported device: $DEVICE"
+        exit 1
+        ;;
+esac
+if [ -z "$OUT" ]; then
+    echo "Please specify out directory."
+    exit 1
+fi
+if [ -d "$OUT" ]; then
+    OUT="$(realpath $OUT)"
+    if ! rm -rf "$OUT"; then
+        echo "Failed to cleanup out directory."
+        exit 1
+    fi
+fi
+if ! mkdir -p "$OUT"; then
+    echo "Failed to create out directory."
+    exit 1
+fi
+if [ -z "$LLVM_PATH" ] || ! [ -f "$LLVM_PATH/bin/clang" ]; then
+    echo "LLVM Toolchain path is wrong."
+    exit 1
+fi
+LLVM_PATH="$(realpath $LLVM_PATH)"
+if ! [ -z "$ARTIFACT_COPY" ] && ! [ -d "$ARTIFACT_COPY" ]; then
+    echo "Artifact copy out directory does not exist."
+    exit 1
+fi
+case "$ARTIFACT_UPLOAD" in
+    "oshi.at"|"transfer.sh")
+        ;;
+    "")
+        ;;
+    *)
+        echo "Unsupported artifact upload site: $ARTIFACT_UPLOAD"
+        exit 1
+        ;;
+esac
+if ! [ -z "$CHERRY_PICK" ]; then
+    if ! echo -n "$CHERRY_PICK"|grep -E "^[a-z0-9| ]+$" > /dev/null; then
+        echo "Invalid commit ids to cherry-pick: $CHERRY_PICK"
+        exit 1
+    fi
+fi
+case "$PARTITION" in
+    "boot")
+        ;;
+    "recovery")
+        LEGACY_VIB="true"
+        ;;
+    "")
+        PARTITION="boot"
+        ;;
+    *)
+        echo "Unsupported partition: $PARTITION"
+        exit 1
+        ;;
+esac
+
+if ! [ -z "$CHERRY_PICK" ]; then
+    VARIANT_NAME="TEST"
+elif [ "$PARTITION" == "recovery" ]; then
+    VARIANT_NAME="Recovery"
+elif [ "$LEGACY_OMX" == "true" ] && [ "$LEGACY_VIB" == "true" ]; then
+    VARIANT_NAME="LegacyOMXVib"
+elif [ "$LEGACY_OMX" == "true" ]; then
+    VARIANT_NAME="LegacyOMX"
+elif [ "$LEGACY_VIB" == "true" ]; then
+    VARIANT_NAME="LegacyVibrator"
+fi
+
+git stash save -a "$(date +backup_%Y%m%d-%H%M%S)" || true
+git checkout $BRANCH_MAIN
+git checkout $(git rev-parse --short HEAD)
+
+if ! [ -z "$CHERRY_PICK" ]; then
+    if ! eval git cherry-pick $CHERRY_PICK; then
+        echo "Failed to cherry-pick specified commits"
+        git reset --hard
+    fi
+fi
+if [ "$LEGACY_OMX" == "true" ]; then
+    git cherry-pick $(func_get_commitid_by_msg "$COMMITMSG_LEGACY_OMX")
+fi
+if [ "$LEGACY_VIB" == "true" ]; then
+    git cherry-pick $(func_get_commitid_by_msg "$COMMITMSG_LEGACY_VIB_GPIO")
+    git cherry-pick $(func_get_commitid_by_msg "$COMMITMSG_LEGACY_VIB_QPNP")
+fi
+
+if [ "$PARTITION" == "recovery" ]; then
+    sed -i 's|max_brightness = LED_FULL|max_brightness = 1|g' drivers/leds/leds-msm-back-gpio-flash-ulysse.c
+    git add drivers/leds/leds-msm-back-gpio-flash-ulysse.c
+    git commit -m "Workaround flashlight issue in recovery mode"
+fi
+
+cp arch/arm64/configs/${DEVICE}_defconfig $OUT/.config
+source $OUT/.config
+if ! [ -z "$VARIANT_NAME" ]; then
+    sed -i "s|CONFIG_LOCALVERSION=\"${CONFIG_LOCALVERSION}\"|CONFIG_LOCALVERSION=\"${CONFIG_LOCALVERSION}-${VARIANT_NAME}\"|g" $OUT/.config
+fi
+echo "# Appended by build script" >> $OUT/.config
+if [ "$LTO" == "true" ]; then
+    func_set_defconfig $OUT/.config CONFIG_LTO_CLANG y
+fi
+source $OUT/.config
+
+export ARCH=arm64
+export PATH="${LLVM_PATH}/bin:$PATH"
+export CROSS_COMPILE="${LLVM_PATH}/bin/aarch64-linux-gnu-"
+export CROSS_COMPILE_ARM32="${LLVM_PATH}/bin/arm-linux-gnueabi-"
+
+make O=$OUT $BUILD_FLAGS olddefconfig
+make O=$OUT $BUILD_FLAGS
+
+BUILD_DATE_SHORT="$(TZ=UTC date +%Y%m%d)"
+KERNEL_VERSION="$(grep 'Kernel Configuration' $OUT/.config | cut -d ' ' -f 3)"
+LOCALVERSION="$(echo -n $CONFIG_LOCALVERSION|cut -c 2-)"
+
+if ! [ -f "$OUT/arch/arm64/boot/Image.gz" ] || ! [ -f "$OUT/arch/arm64/boot/Image.gz-dtb" ]; then
+    echo "Kernel binary has not generated"
+    exit 1
+fi
+
+if [ -d ".ak3_patches" ] && ! [ -z "$(ls .ak3_patches/*.patch)" ]; then
+    git am .ak3_patches/*.patch
+fi
+if [ -d ".ak3_patches/device/${DEVICE}" ] && ! [ -z "$(ls .ak3_patches/device/${DEVICE}/*.patch)" ]; then
+    git am .ak3_patches/device/${DEVICE}/*.patch
+fi
+sed -i "s|REPLACE_KERNEL_STRING|${LOCALVERSION} Kernel ${KERNEL_VERSION}|g" .ak3/anykernel.sh
+sed -i "s|REPLACE_DEVICE_NAME|${DEVICE}|g" .ak3/anykernel.sh
+sed -i "s|REPLACE_PARTITION|${PARTITION}|g" .ak3/anykernel.sh
+if [ "$PARTITION" == "boot" ]; then
+    sed -i "s|REPLACE_ANDROID_VERSION|${SUPPORTED_ANDROID_VERSIONS}|g" .ak3/anykernel.sh
+else
+    sed -i "s|REPLACE_ANDROID_VERSION||g" .ak3/anykernel.sh
+fi
+rm -rf $OUT/pack
+cp -r .ak3 $OUT/pack
+
+git add -A || true
+git commit -m "Final changes of build on $(date)" || true
+FINAL_GIT_HEAD_SHORT="$(git rev-parse --short HEAD)"
+
+cd $OUT/pack
+mv ../arch/arm64/boot/Image.gz-dtb Image.gz-dtb
+cp ../.config kernel-config.txt
+ARTIFACT_NAME="${LOCALVERSION}-Kernel-${KERNEL_VERSION}-${BUILD_DATE_SHORT}-${FINAL_GIT_HEAD_SHORT}.zip"
+zip -r9 "../$ARTIFACT_NAME" * -x .git README.md *placeholder *.zip
+cd ../
+
+if [ -f "$ARTIFACT_NAME" ]; then
+    if [ -d "$ARTIFACT_COPY" ]; then
+        cp $ARTIFACT_NAME ${ARTIFACT_COPY}/
+    fi
+    case "$ARTIFACT_UPLOAD" in
+        "oshi.at")
+            curl --upload-file $ARTIFACT_NAME https://oshi.at/${ARTIFACT_NAME} | tee upload-oshi_at.txt
+            ;;
+        "transfer.sh")
+            curl --upload-file $ARTIFACT_NAME https://transfer.sh/${ARTIFACT_NAME} | tee upload-transfer_sh.txt
+            ;;
+    esac
+fi
+
+exit 0
