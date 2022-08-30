d693c8bccf5c226d442f6c10338a2db9a0fb777c
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-31 05:12:08 +0800

    uapi/media: Checkout msm_media_info.h to previous merge
    `MSM_MEDIA_MAX(extra_size` got added from nowhere...

--- include/uapi/media/msm_media_info.h
+++ include/uapi/media/msm_media_info.h
@@ -1347,8 +1347,7 @@ static inline unsigned int VENUS_BUFFER_SIZE(
 			uv_meta_scanlines, 4096);
 
 		size = (y_ubwc_plane + uv_ubwc_plane + y_meta_plane +
-			uv_meta_plane)*2 +
-			MSM_MEDIA_MAX(extra_size + 8192, 48 * y_stride);
+			uv_meta_plane)*2;
 		size = MSM_MEDIA_ALIGN(size, 4096);
 
 		/* Additional size to cover last row of non-aligned frame */
